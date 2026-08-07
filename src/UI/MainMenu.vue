7f33e136591ca1539da06b46c9a288eb5d8f3af5
chiaramellomanuel <mennymachi@gmail.com>
2026-08-07 15:06:00 +0200

    feat: main menu buttons are now carousel

--- src/UI/MainMenu.vue
+++ src/UI/MainMenu.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+	import { ref, onMounted, onUnmounted } from 'vue';
 	import { useGameStore } from '../stores/gameStore';
 
 	const	gameStore = useGameStore();
+	const	selectedIndex = ref(0);
+	let		lastMoveTime = 0;
+	const	moveCooldown = 100;
 
 	const	menuButtons = [
 		{ label: 'Play', action: 'play' },
@@ -18,25 +22,63 @@
 		}
 	};
 
+	const	handleKeyDown = (e: KeyboardEvent) => {
+		const	now = Date.now();
+
+		if ((now - lastMoveTime) < moveCooldown) return;
+
+		if (e.key === 'ArrowDown') {
+			selectedIndex.value = (selectedIndex.value + 1) % menuButtons.length;
+			lastMoveTime = now;
+		}
+		else if (e.key === 'ArrowUp') {
+			selectedIndex.value = (selectedIndex.value - 1 + menuButtons.length) % menuButtons.length;
+			lastMoveTime = now;
+		}
+		else if (e.key === 'Enter')
+			handleMenuClick(menuButtons[selectedIndex.value].action);
+	};
+
+	const	getItemStyle = (index: number) => {
+		const	distance = index - selectedIndex.value;
+		const	translateY = `calc(-50% + ${distance * 80}px)`;
+		const	scale = distance === 0 ? 1 : 0.5;
+		const	opacity = distance === 0 ? 1 : (Math.abs(distance) === 1 ? 0.4 : 0);
+
+		return {
+			top: '50%',
+			transform: `translateY(${translateY}) scale(${scale})`,
+			opacity: opacity,
+			zIndex: distance === 0 ? 10 : 5
+		};
+	};
+
+	onMounted(() => {
+		window.addEventListener('keydown', handleKeyDown);
+	});
+
+	onUnmounted(() => {
+		window.removeEventListener('keydown', handleKeyDown);
+	})
+
 </script>
 
 <template>
-	<div class="absolute flex flex-col items-center justify-evenly z-20 w-full h-full">
-		<div class="flex items-center justify-center">
-			<div class="text-yellow-300 text-9xl font-mono">
-				PacMan3D
-			</div>
+	<div class="absolute flex flex-col items-center justify-start z-20 w-full h-full bg-black overflow-hidden">
+		<div class="mt-20 mb-10 text-yellow-300 text-9xl font-mono z-30 drop-shadow-[0_0_20px_rgba(250,204,21,0.5)]">
+			PacMan3D
 		</div>
-		<div class="flex flex-col items-center justify-center text-7xl space-y-4 text-yellow-300">
+
+		<div class="relative w-full h-125 mt-10">
 			<button
-				v-for="btn in menuButtons"
+				v-for="(btn, index) in menuButtons"
 				:key="btn.label"
+				:style="getItemStyle(index)"
 				@click="handleMenuClick(btn.action)"
-				class="transition-all duration-100 hover:cursor-pointer hover:scale-110 active:scale-90"
+				class="absolute w-full text-center text-7xl font-mono text-yellow-300 transition-all duration-300 ease-out hover:cursor-pointer outline-none"
 			>
 				{{ btn.label }}
 			</button>
 		</div>
-
 	</div>
 </template>
