9223bf924623b5a9423dc521030d75d1aca7ce67
chiaramellomanuel <mennymachi@gmail.com>
2026-09-05 12:52:57 +0200

    fix: removed gameStore from app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import GameExperience from './components/GameExperience.vue'
 import GameHUD from './components/GameHUD.vue'
-import { useGameStore } from './stores/gameStore'
-
-const gameStore = useGameStore()
 </script>
 
 <template>
